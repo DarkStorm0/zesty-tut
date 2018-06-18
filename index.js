4ecd2e0872c7cb514699c158749aa866952d8e42
DarkStorm0 <39872618+DarkStorm0@users.noreply.github.com>
2018-06-18 22:01:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ bot.on("message", async message => {
   if (cmd === `${prefix}storm`){
     message.channel.send("Storm is our lord!");
  
+    if (cmd === `${prefix}storm2`){
+    message.channel.send("Storm is our lord2!");
+
   
   }
 });
